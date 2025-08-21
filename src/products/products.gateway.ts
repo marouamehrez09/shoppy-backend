a0548bed625a8399448d894fc5cf39fc3c8dda79
marouamehrez09 <maroua.mehrez09@gmail.com>
2025-08-21 13:06:45 +0200

    cors fix

--- src/products/products.gateway.ts
+++ src/products/products.gateway.ts
@@ -1,10 +1,16 @@
-import { WebSocketGateway, WebSocketServer, WsException } from '@nestjs/websockets';
+import {
+  WebSocketGateway,
+  WebSocketServer,
+  WsException,
+} from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 import { AuthService } from 'src/auth/auth.service';
 
 @WebSocketGateway({
   cors: {
-    origin: '*',
+    origin: 'https://shoppy-ui-app.vercel.app', // Explicitly allow your frontend origin
+    methods: ['GET', 'POST'],
+    credentials: true, // If your auth requires cookies or credentials
   },
 })
 export class ProductGateway {
@@ -21,7 +27,7 @@ export class ProductGateway {
     try {
       this.authService.verifyToken(client.handshake.auth.Authentication.value);
     } catch (err) {
-      throw new WsException('Unauthorized.')
+      throw new WsException('Unauthorized.');
     }
   }
 }
